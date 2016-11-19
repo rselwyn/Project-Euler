e64a9587d61817b9cbc32885fee99442143e47e7
Robbie Selwyn <robbie@selwynsoftware.com>
2016-11-18 22:42:18 -0800

    Problem 12 too

---
+++ Problems/12.py
@@ -0,0 +1,12 @@
+def factors(n):    
+    return reduce(list.__add__, 
+                ([i, n//i] for i in range(1, int(n**0.5) + 1) if n % i == 0))
+
+numToAdd = 2
+currentSum = 1
+
+while len(factors(currentSum)) < 500:
+	currentSum += numToAdd
+	numToAdd+=1
+
+print currentSum # 76576500
